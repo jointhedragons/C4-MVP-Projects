5d77822241586deccb42311c121b423489d8e654
mo7medA <modyayman246@gmail.com>
2025-09-23 20:58:53 +0300

    create my coursesPage

--- ِai-lms/src/pages/CoursesPage.jsx
+++ ِai-lms/src/pages/CoursesPage.jsx
@@ -1,6 +1,7 @@
 import { useAuth } from "../contexts/AuthContext";
 
 import LoginMessage from "../components/LoginMessage";
+import { FaBookOpen } from "react-icons/fa";
 
 function CoursesPage() {
   const { isAuth, user } = useAuth();
@@ -8,7 +9,24 @@ function CoursesPage() {
   console.log(user);
 
   if (!isAuth) return <LoginMessage />;
-  return <div></div>;
+
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-2xl font-bold text-gray-900 mb-12">My Courses</h1>
+
+      <div className="flex flex-col items-center justify-center text-center">
+        <FaBookOpen className="w-16 h-16 text-gray-400 mb-4" />
+
+        <h2 className="text-lg font-semibold text-gray-800">
+          No courses enrolled yet
+        </h2>
+
+        <p className="text-gray-600 mt-1">
+          Start learning by enrolling in courses from the home page.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default CoursesPage;
